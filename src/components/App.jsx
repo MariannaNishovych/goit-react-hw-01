6d06b4c8a569b703d7583f5674d56e9fa68662e0
Marianna Nishovych <nisovicmarianna@gmail.com>
2024-10-23 16:44:34 +0300

    add profile

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+import Profile from './Profile/Profile'
+import userData from "../userData.json"
+
+
+function App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App
